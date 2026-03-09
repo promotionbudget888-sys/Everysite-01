31ad6bceffe8b9370803ee916b512596bb42e876
promotionbudget888-sys <promotion.budget888@gmail.com>
2026-03-09 20:22:10 +0700

    Update CreateRequest.tsx

--- src/pages/CreateRequest.tsx
+++ src/pages/CreateRequest.tsx
@@ -31,7 +31,7 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 type RequestType = "matching_fund" | "everysite";
 
-const REQUEST_FORM_VIEW_URL = "https://drive.google.com/file/d/1z2HPMUC_0fs1-dbwXTDcwGUOZygQYbqm/view?usp=sharing";
+const REQUEST_FORM_VIEW_URL = "https://drive.google.com/file/d/1wK63DQETRq61yHjQ-wmS2uO5B-T8fQhx/view?usp=sharing";
 
 const formSchema = z.object({
   title: z.string().min(5, "ชื่อโครงการต้องมีอย่างน้อย 5 ตัวอักษร").max(200),
